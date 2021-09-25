40b46928db33f82b85862a13302cadce192a5bf6
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-09-25 22:15:30 +0100

    chore: updating README.md

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,9 @@
 const app = new Vue({
   el: '#app',
   data: {
-    jobs: []
+    jobs: [],
+    jobsListed: [],
+    tagsToSearch: []
   },
   methods: {
     fetchAllJobs: function () {
@@ -9,10 +11,40 @@ const app = new Vue({
       .then((data) => data.json())
       .then((data) => {
         this.jobs = data;
+        this.jobsListed = data;
       })
       .catch((err) => {
         this.jobs = []
       })
+    },
+    deleteAllSearchFilterTags: function () {
+      this.tagsToSearch = [];
+      this.fetchAllJobs();
+    },
+    addTagSearchFilter: function (tag) {
+      if (!this.tagsToSearch.includes(tag)) {
+        this.tagsToSearch.push(tag);
+        this.filterJobs();
+      }
+    },
+    deleteTagToTheSearchFilter: function (tag) {
+      this.tagsToSearch = this.tagsToSearch.filter(tagText => tag !== tagText)
+      this.fetchAllJobs();
+    },
+    filterJobs: function() {
+      let tempJobs = [];
+
+      for (const job of this.jobs) {
+        if (this.isJobInFilterRequisites([job.role, job.level, ...job.tools, ...job.languages])) {
+          tempJobs.push(job);
+        }
+      }
+      this.jobsListed = tempJobs;
+    },
+    isJobInFilterRequisites: function (jobNecessities) {
+      const tagsThatJobHas = this.tagsToSearch.filter(tag => jobNecessities.includes(tag));
+
+      return tagsThatJobHas.length == this.tagsToSearch.length;
     }
   }
 })
